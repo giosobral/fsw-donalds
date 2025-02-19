614876facc5d4770f348df92a6412587d9be3146
Jhowman037 <giovanesobral10@hotmail.com>
2025-02-19 19:38:04 -0300

    feat - restaurant initial page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+
+  const restaurant = await getRestaurantBySlug(slug);
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como irá preferir saborear a sua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          imageUrl="/dine_in.png"
+          imageAlt="Para comer aqui"
+          buttonText="Para comer Aqui"
+          option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+          imageUrl="/take_away.png"
+          imageAlt="Para Levar"
+          buttonText="Para levar"
+          option="TAKEAWAY"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
